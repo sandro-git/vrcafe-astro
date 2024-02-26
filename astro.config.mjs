c4c57a9f850229b61b45aa5583b8c349ab02a2b9
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2024-02-26 01:56:25 +0100

    netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import { sanityIntegration } from "@sanity/astro";
 // import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
     dataset: "production",
     // Set useCdn to false if you're building statically.
     useCdn: false,
-    studioBasePath:"/admin"
-  }), react()]
-
+    studioBasePath: "/admin"
+  }), react()],
+  adapter: netlify()
 });
